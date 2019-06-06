d46e1f0a338276c6e0db575d38c209834a4a730e
Power___ <47273334+Power1200@users.noreply.github.com>
2019-06-06 06:56:23 -0700

    Add files via upload

---
+++ Basic-Math-With-Python-Numbers.py
@@ -0,0 +1,22 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Operations with Numbers
+12+34
+46
+>>> 3-4
+-1
+>>> 2/4
+0.5
+>>> 100/50
+2.0
+>>> 11%2
+1
+>>> 10%2
+0
+>>> 50%5
+0
+>>> 3**2
+9
+>>> 4**8
+65536
+>>> 
